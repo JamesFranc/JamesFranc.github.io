dec94a03bf45644ba7fe09fde0caf71b968de6f0
James Francis <francis.jamesii@gmail.com>
2017-02-04 18:57:40 -0600

    merging into master, removed console logging

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,5 @@
 window.onscroll = function(){
-  console.log(window.pageYOffset);
+  // console.log(window.pageYOffset);
   var nav = document.getElementById('nav');
   if(window.pageYOffset >450){
     nav.classList.add("navbar1");
