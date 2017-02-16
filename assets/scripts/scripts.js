8096928506bd92c1b35bb5877c9fcef218e0490d
James Francis <francis.jamesii@gmail.com>
2017-02-16 10:04:02 -0600

    more nav modifications

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,12 @@
 
 window.onscroll = function(){
   // console.log(window.pageYOffset);
-  console.log(window.pageYOffset);
   var nav = document.getElementById('nav');
-  if(window.pageYOffset >450){
+  if(window.pageYOffset >500){
     nav.classList.remove("nav-up");
     nav.classList.add("nav-down");
       }
-      else if(window.pageYOffset <450){
+      else if(window.pageYOffset <500){
         nav.classList.remove("nav-down");
         nav.classList.add("nav-up");
       }
