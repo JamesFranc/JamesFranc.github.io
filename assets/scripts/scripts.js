6af305ed39bd80ba3547f036d6807032ba225255
James Francis <francis.jamesii@gmail.com>
2017-02-16 11:48:05 -0600

    fixed navbar styling

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,13 +1,11 @@
-
-window.onscroll = function(){
-  // console.log(window.pageYOffset);
-  var nav = document.getElementById('nav');
-  if(window.pageYOffset >500){
-    nav.classList.remove("nav-up");
-    nav.classList.add("nav-down");
-      }
-      else if(window.pageYOffset <500){
+window.onscroll = function() {
+    // console.log(window.pageYOffset);
+    var nav = document.getElementById('nav');
+    if (window.pageYOffset > 500) {
+        nav.classList.remove("nav-up");
+        nav.classList.add("nav-down");
+    } else if (window.pageYOffset < 500) {
         nav.classList.remove("nav-down");
         nav.classList.add("nav-up");
-      }
     }
+}
